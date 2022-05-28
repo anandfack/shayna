b28a542e2a6029d63afe5ccfa848b81509a15bbb
anandfack <anandfirdausyah45@gmail.com>
2022-05-28 17:52:19 +0700

    upload project vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="home">
+<!-- Memanggil Components -->
+<HeaderShayna />
+<CarouselShayna />
+<WomenBannerShayna />
+<InstaShayna />
+<PartnerLogoShayna />
+<FooterShayna />
+<!-- Akhir Memanggil Components -->
+</div>
+</template>
+
+<script>
+// Import Name dari file Components
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import CarouselShayna from '@/components/CarouselShayna.vue'
+import WomenBannerShayna from '@/components/WomenBannerShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogoShayna from '@/components/PartnerLogoShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    WomenBannerShayna,
+    InstaShayna,
+    PartnerLogoShayna,
+    FooterShayna
+}
+}
+</script>
---
+++ src/views/SuccessView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="success">
+        <SuccessShayna />
+    </div>
+</template>
+
+<script>
+import SuccessShayna from '@/components/SuccessShayna.vue'
+export default {
+    name: 'SuccessView',
+    components: {
+        SuccessShayna,
+    }
+}
+</script>
